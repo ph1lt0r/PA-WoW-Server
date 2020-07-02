4a3055d03fc2e5ec0018c7f72ad3946ab569a5d0
Phil Sortland <psortland@gmail.com>
2020-07-02 13:14:50 -0500

    First commit Backup script started

---
+++ 1. Backup/wow-backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+######################################################################################
+#
+#   Script Name: wow-backup.sh
+#
+#   Author: Phil "Ph1Lt0r" Sortland
+#
+#   Description: used to backup the three databases for a docker WoW server
+#
+######################################################################################
+#
+#   Version: 1.0.0 - Initial release
+#
+######################################################################################
+
+function dbBackup() {
+    
+}
